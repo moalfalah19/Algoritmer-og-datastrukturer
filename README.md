736518af8f32cfb08bdc130a92af8cd6c3bc5d28
s362092 <89576230+s362092@users.noreply.github.com>
2022-10-26 11:09:27 +0200

    Jeg har kopierete koden 5.2.3a fra kompediet, men jeg fikk feil når jeg testet koden på p = new Node <>(verdi), på konstruktøren til Node<T> på kildekode er det to parametre (verdi og forealderen). På koden er q foralder, så testet jeg oppgaven og fikk riktig svar. På koden brukes to nodereferanse p,q. Referansen P starter i rotnode, den flyttes på nedover i treet venstre om verdi er mindre, og høyre om verdi er større, så brukte jeg Compare-metoden for å sammenligne. q skal passere p og blir siste noden, da skal den nye verdi legges og sammenlignes med q og setter som barn, enten til høyre eller til venstre.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Denne oppgaven er en innlevering i Algoritmer og Datastrukturer. 
 Oppgaven er levert av følgende student:
-* Navn Navnesen, S123456, s123456@oslomet.no
+*  Mohanad Al-Falah, S362092, s362092@oslomet.no
 
 
 # Oppgavebeskrivelse
--- src/main/java/no/oslomet/cs/algdat/Oblig3/SBinTre.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig3/SBinTre.java
@@ -87,26 +87,26 @@ public class SBinTre<T> {
 
         Objects.requireNonNull(verdi, "Ulovlig med nullverdier!");
 
-        Node<T> p = rot, q = null;               // p starter i roten
-        int cmp = 0;                             // hjelpevariabel
+        Node<T> p = rot, q = null; // p starter i roten
+        int cmp = 0; // hjelpevariabel
 
-        while (p != null)       // fortsetter til p er ute av treet
+        while (p != null) // fortsetter til p er ute av treet
         {
-            q = p;                                 // q er forelder til p
-            cmp = comp.compare(verdi,p.verdi);     // bruker komparatoren
-            p = cmp < 0 ? p.venstre : p.høyre;     // flytter p
+            q = p; // q er forelder til p
+            cmp = comp.compare(verdi,p.verdi); // bruker komparatoren
+            p = cmp < 0 ? p.venstre : p.høyre; // flytter p
         }
 
         // p er nå null, dvs. ute av treet, q er den siste vi passerte
 
-        p = new Node<>(verdi,q);                   // oppretter en ny node
+        p = new Node<>(verdi,q); // oppretter en ny node
 
-        if (q == null) rot = p;                  // p blir rotnode
-        else if (cmp < 0) q.venstre = p;         // venstre barn til q
-        else q.høyre = p;                        // høyre barn til q
+        if (q == null) rot = p; // p blir rotnode
+        else if (cmp < 0) q.venstre = p; // venstre barn til q
+        else q.høyre = p; // høyre barn til q
 
-        antall++;                                // én verdi mer i treet
-        return true;                             // vellykket innlegging
+        antall++; // én verdi mer i treet
+        return true; // vellykket innlegging
     }
 
     public boolean fjern(T verdi) {
